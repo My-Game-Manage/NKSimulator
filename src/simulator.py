a3fe150802a47d4cb0a7447da9b18f2a9f94d6bf
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-29 21:04:49 +0900

    Update simulator.py

--- src/simulator.py
+++ src/simulator.py
@@ -1,1 +1,22 @@
-# simulator.py
+"""
+simulator.py の概要
+
+1. レースをシミュレートするために動的にCSVデータからコース、騎手、馬のデータを構築する
+2. RaceEngineを使い、1フレーム毎の挙動を計算して、馬を動かす
+"""
+
+import os
+import time
+
+from src.utils.logger import setup_logger
+
+class RaceSimulator:
+    def __init__(self):
+        """
+        初期化：
+        """
+        _CLASSNAME = "Simulator"
+        # クラス名を名前としてロガーを作成
+        self.logger = setup_logger(_CLASSNAME)
+
+        self.logger.info("初期化しています...")
