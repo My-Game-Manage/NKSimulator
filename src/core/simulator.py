262e587f61c4aff7f66ff154a35344672c21437e
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-30 20:39:44 +0900

    Implement RaceSimulator class with logging setup
    Add initial implementation of RaceSimulator class with logging.

--- src/core/simulator.py
+++ src/core/simulator.py
@@ -1,1 +1,25 @@
+"""
+simulator.py の概要
 
+1. 会場データの初期化
+2. 各馬のエントリー
+3. 実行管理：RaceEngine でゴールするまでループを回す
+4. ログなどの記録
+"""
+from src.utils.logger import setup_logger
+
+class RaceSimulator:
+    def __init__(self):
+        _CLASSNAME = "Simulator"
+        # クラス名を名前としてロガーを作成
+        self.logger = setup_logger(_CLASSNAME)
+
+        self.logger.info("初期化中...")
+
+    def run(self, target_date=None, course_filter=None, race_num_filter=None):
+        """
+        メインの実行メソッド
+        """
+        self.logger.info("実行中...")
+        pass
+        
