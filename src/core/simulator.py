0e8f6edecc98075b2dcedb5250dfc6f97862887e
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-31 16:34:14 +0900

    Update simulator.py

--- src/core/simulator.py
+++ src/core/simulator.py
@@ -51,7 +51,7 @@ class RaceSimulator:
                 # インスタンス化済みのfactoryから馬を生成
                 horse = self._entry_horse(horse_factory, row)
                 horses.append(horse)
-                self.logger.info(f"appended horse: {horse.horse_name}/{horse.horse_id}/{horse.horse_params}")
+                self.logger.info(f"appended horse: {horse.name}/{horse.horse_id}/{horse.params}")
             
             # 馬がいない場合は次のレースへ
             if not horses: continue
