6a00463a18a9bdd249e311fea0af567017963e8f
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-04-01 23:28:19 +0900

    Update config.py

--- src/constants/config.py
+++ src/constants/config.py
@@ -30,7 +30,7 @@ class SimConfig:
     # --- スタミナ関連 ---
     # スタミナ消費（0.008 -> 0.012 に引き上げ：終盤に足を鈍らせる）
     STAMINA_LOSS_COEFF = 0.0485
-    EXHAUST_SPEED_COEFF = 0.8  # スタミナ切れ時の速度倍率
+    EXHAUSTED_SPEED_COEFF = 0.8  # スタミナ切れ時の速度倍率
     CONSUMPTION_RATE = 2.2
     SPART_MULTIPLIER = 1.5
     NORMAL_SPART_MULTIPLIER = 1.0
